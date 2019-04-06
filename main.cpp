ebefc97f638d3a5f7238233a5974c56d065990fa
Hudson <hthumphries@gmail.com>
2019-04-06 15:56:35 -0500

    Moved function to helper and added intermediate file after one pass

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include "helper.h"
 using namespace std;
-
-bool emptyLine(string s) {
-    for(int i = 0; i < s.size(); i++) {
-        if(!(isspace(s[i]))) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main(int argc, char **argv) {
     cout << argv[1] << endl;
     string line;
     ifstream myfile(argv[1]);
+    ofstream myfile2("middle.asm");
     if(myfile.is_open()) {
         while(getline(myfile,line)) {
             if(emptyLine(line) == false && !(line[0] == '/' && line[1] == '/')) {
-                cout << line << endl;
+                myfile2 << line << endl;
+                //Add to file?
+            }
+            else {
+                //if label, symboltable
             }
             
         }
         myfile.close();
+        myfile2.close();
     }
 }
